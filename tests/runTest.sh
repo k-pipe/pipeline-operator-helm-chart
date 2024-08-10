3102cbd8a4042a9960b44da7169ccf2741c20234
Jan Kneissler <jan.kneissler@breuninger.de>
2024-08-10 19:50:37 +0100

    log test

--- tests/runTest.sh
+++ tests/runTest.sh
@@ -14,6 +14,7 @@ then
   exit 1
 fi
 
+sleep 5
 echo Running checks
 bash "$1"check.sh
 if [[ $? != 0 ]]
@@ -23,12 +24,13 @@ then
 fi
 
 echo Checking logs
-kubectl logs deployment/k-pipe-operator -n k-pipe  | grep -v 'INFO' | grep -v 'DEBUG' | grep -v '^I' | grep -v '^Trace.*END$' > errlogs.txt
+kubectl logs deployment/k-pipe-operator -n k-pipe > logs.txt
 if [[ $? != 0 ]]
 then
-  echoerr Checks failed
+  echoerr Logs could not be read
   exit 1
 fi
+cat logs.txt | grep -v 'INFO' | grep -v 'DEBUG' | grep -v '^I' | grep -v '^Trace.*END$' > errlogs.txt
 if [[ `grep -c '' errlogs.txt` != 0 ]]
 then
   echoerr "Detected errors in operator logs"
