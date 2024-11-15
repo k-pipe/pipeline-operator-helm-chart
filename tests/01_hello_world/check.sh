137a723b2e520a4bfd74bfcbe077ad93e36c0eea
Jan Kneissler <jan.kneissler@breuninger.de>
2024-11-15 07:54:37 +0100

    update pipeline roles

--- tests/01_hello_world/check.sh
+++ tests/01_hello_world/check.sh
@@ -16,5 +16,7 @@ echo waiting for pipeline to succeed
 kubectl wait --for=condition=Succeeded --timeout=500s pr/test-run
 if [[ $? != 0 ]]
 then
+   echo "Pipeline did not succeed:"
+   kubectl get pr test-run
    exit 1
 fi
